8bbd8ced3e4f24926a3cbb893066dc4d63401a6d
Gaël Marcadet <gamarcadet@gmail.com>
2026-01-27 17:00:30 +0100

    feat: implement max block size checks and early microblock rejection service
    - Added `max_block_size_in_bytes` configuration to limit transaction blocks.
    - Integrated an `EarlyMicroblockRejectionService` for early validation of microblocks.
    - Enhanced microblock sorting by gas consumption and implemented size-based block finalization in `AbciService`.
    - Updated dependencies to include `@nestjs/schedule` and `cron` for scheduled tasks.

---
+++ abci/src/carmentis/abci/services/EarlyMicroblockRejectionService.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { Microblock } from '@cmts-dev/carmentis-sdk/server';
+import { Cron } from '@nestjs/schedule';
+import { getLogger } from '@logtape/logtape';
+
+@Injectable()
+export class EarlyMicroblockRejectionService {
+    private logger = getLogger(['node', 'early-rejection']);
+    private shouldReject: Set<string> = new Set();
+
+    markMicroblockAsRejected(microblock: Microblock) {
+        this.logger.info(`Marking microblock ${microblock.getHash().encode()} as to be rejected`);
+        this.shouldReject.add(microblock.getHash().encode());
+    }
+
+    shouldBeRejected(microblock: Microblock) {
+        return this.shouldReject.has(microblock.getHash().encode());
+    }
+
+    @Cron('*/10 * * * * *')
+    clearRejectedMicroblocks() {
+        this.logger.info(`Clearing rejected microblocks (${this.shouldReject.size} entries)`);
+        this.shouldReject.clear();
+    }
+
+}
